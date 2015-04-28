2bea3d9eb81a096f4a8284e8d71d2c93f3be8fbf
Ismael Ambrosi <ismaambrosi@gmail.com>
2015-04-28 00:40:10 -0300

    Cache de templates para el entorno prod

--- config/dev.php
+++ config/dev.php
@@ -4,5 +4,7 @@
 return function (Silex\Application $app) {
     $config = call_user_func(require __DIR__.'/prod.php', $app);
 
+    $app['twig.options'] = ['cache' => false];
+
     return $config;
 };
--- config/prod.php
+++ config/prod.php
@@ -3,7 +3,9 @@
 
 return function (Silex\Application $app) {
 
-    $app['twig.path']       = __DIR__.'/../src/App/Resources/views';
+    $app['twig.path']    = __DIR__.'/../src/App/Resources/views';
+    $app['twig.options'] = ['cache' => __DIR__.'/../var/cache/twig'];
+
     $app['locale_fallback'] = 'es';
 
     return [];
