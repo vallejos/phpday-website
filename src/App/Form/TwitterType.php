34a7dd9a4c3728abbab1ba5def0cd766ed245121
Ismael Ambrosi <ismaambrosi@gmail.com>
2015-05-24 03:06:27 -0300

    Form type para usuario de twitter

---
+++ src/App/Form/TwitterType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+
+/**
+ * Class TwitterType.
+ */
+class TwitterType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'constraints' => [
+                new Regex([
+                    'pattern' => '/^[A-Za-z0-9_]{1,15}$/',
+                    'message' => 'Usuario de twitter inválido.',
+                ]),
+            ],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'twitter';
+    }
+}
