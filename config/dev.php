de0ae4d37b9acaa200969387f25ab669e737279d
Ismael Ambrosi <ismaambrosi@gmail.com>
2015-06-05 09:37:41 -0300

    Deshabilita slack en dev

--- config/dev.php
+++ config/dev.php
@@ -1,10 +1,15 @@
 <?php
 
-
 return function (Silex\Application $app) {
     $config = call_user_func(require __DIR__.'/prod.php', $app);
 
     $app['twig.options'] = ['cache' => false];
+
+    $app->extend('slack', function (\App\Notification\Slack $slack) {
+        $slack->disable();
+
+        return $slack;
+    });
 
     return $config;
 };
